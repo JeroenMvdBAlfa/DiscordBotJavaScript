4c3bf383259e60b5ecca11cf3a4133e131a5554d
jcmvdb <mvdb.jeroen@gmail.com>
2022-05-07 17:04:01 +0200

    remove
    made a mistake

--- src/permissions.js
+++
@@ -1,9 +0,0 @@
-// function roleControl(message) {
-//     const memberRoles = message.member.roles.cache
-//         .filter((roles) => roles.id !== message.guild.id)
-//         .map((roles) => roles.toString());
-//     const roleAdmin = message.guild.roles.cache.find(roles => roles.name === "admin");
-//
-//     let control = 0;
-//     memberRoles.forEach(function (element) {if (element === '<@&' + roleAdmin + '>') {control += 1;} else {control += 0;}});
-// }
